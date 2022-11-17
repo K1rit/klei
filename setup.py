8b9f41422f2b265b49c2874e7e5b57ff81621750
OrthodoxEgo <vgtrofimov@gmail.com>
2022-11-17 21:33:22 +0300

    Гуляй вовсю, json понёсся.

---
+++ setup.py
@@ -0,0 +1,15 @@
+WIDTH = 800
+HEIGHT = 600
+
+level = 0
+state = ["5", "4", "3", "неуд.", "кол", "вас выгнали из школы"]
+state_lives = 0
+# print(state[level_state])
+# score = кол-во букв * 2
+# sub_score = 8
+# I have the computer = 32
+#
+# * **** *** ********
+# q 32 - 8 = 24
+# a
+# * *a** *** *******
--- words.py
+++ words.py
@@ -1,1 +1,10 @@
+from data.json_parser import JSONParser
+from setup import *
+
 print()
+
+worker = JSONParser().get_list("data/database.txt", False)
+
+print(worker[level].words_en)
+print(worker[level].words_ru)
+
