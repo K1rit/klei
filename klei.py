0e0fa42172421b9716e89ca361f8dead351a8ac9
OrthodoxEgo <vgtrofimov@gmail.com>
2022-11-18 21:22:53 +0300

    Добавлены звуки.

--- klei.py
+++ klei.py
@@ -1,10 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 from setup import *
+from services.sound import Sound
 import time
 
 # Окно игры
 def open_play_game():
+    def window_play_game_destroy():
+        Sound().play(Sound.BUTTON_PRESS)
+        window_play_game.destroy()
 
     # Клавиши от A до U
     def dictionary():
@@ -35,11 +39,7 @@ def open_play_game():
                                       width=width_key, height=height_key))
                 buttons[-1].place(x=start_x + j * (width_key * 20), y=HEIGHT // 2 + i * 50)
 
-
-
-
-    
-
+    Sound().play(Sound.START_GAME)
     window_play_game = Toplevel()
     window_play_game.grab_set()
 
@@ -52,12 +52,18 @@ def open_play_game():
     dictionary()
 
 
-    button_exit = Button(window_play_game, text="Сбежать", font=font_button, command = window_play_game.destroy, width=10, pady=3).place(relx=0.85, rely=0.9)
+    button_exit = Button(window_play_game, text="Сбежать", font=font_button, command = window_play_game_destroy, width=10, pady=3).place(relx=0.85, rely=0.9)
     button_question = Button(window_play_game, text="?", font=font_button, command = None, width=7, pady=3).place(relx=0.03, rely=0.9)
 
 
 # Окно авторов
 def open_authors():
+
+    def window_authors_destroy():
+        Sound().play(Sound.BUTTON_PRESS)
+        window_authors.destroy()
+
+    Sound().play(Sound.BUTTON_PRESS)
     window_authors = Toplevel()
     window_authors.grab_set()
 
@@ -67,7 +73,7 @@ def open_authors():
     window_authors["bg"] = MAIN_COLOR
     window_authors.overrideredirect(1)
 
-    button_exit = Button(window_authors, text="ОК", font=font_button, command=window_authors.destroy, width=10).place(relx=0.5, rely=0.90, anchor=CENTER)
+    button_exit = Button(window_authors, text="ОК", font=font_button, command=window_authors_destroy, width=10).place(relx=0.5, rely=0.90, anchor=CENTER)
 
     # KLEI
     label_app_name = ttk.Label(window_authors, text=application_name, font=font_authors_caption, background=MAIN_COLOR,
@@ -85,6 +91,8 @@ def open_authors():
     label_hard_text = ttk.Label(window_authors, text=note_text, font=("Arial", 9, "bold"), background=MAIN_COLOR,
                               foreground=TEXT_COLOR).place(relx=0.03, rely=0.71)
 
+def quit_game():
+    window.quit()
 
 window = Tk()
 window.title("Klei")
@@ -103,7 +111,7 @@ window["bg"] = MAIN_COLOR
 label_version = ttk.Label(text=f"Версия {VERSION}", anchor="sw", background=MAIN_COLOR, foreground=TEXT_COLOR).place(relx=0.03, rely=0.87)
 
 button_game = Button(window, text="Играть", font=font_button, command = open_play_game, width=30, pady=3).place(relx=0.5, rely=0.30, anchor=CENTER)
-button_exit = Button(window, text="Выход", font=font_button, command = window.quit, width=30, pady=3).place(relx=0.5, rely=0.50, anchor=CENTER)
+button_exit = Button(window, text="Выход", font=font_button, command = quit_game, width=30, pady=3).place(relx=0.5, rely=0.50, anchor=CENTER)
 button_authors = Button(window, text="Авторы", font=font_button, command = open_authors, width=30, pady=3).place(relx=0.5, rely=0.70, anchor=CENTER)
 
 
---
+++ services/sound.py
@@ -0,0 +1,35 @@
+# pip install playsound
+import pip
+from threading import Thread
+
+# Здесь проверка на наличие библиотеки для воспроизведения звука
+# Если библиотеки нет, Питон предложит установить её
+# Если не установлена, то никакого звука и не будет
+
+play_sound = True
+try:
+    from playsound import playsound
+except ImportError:
+    print("Библиотека playsound не установлена. Установите её. ")
+    n = input("Установить? (Y/n): ")
+    if n.upper() == "Y":
+        pip.main(["install", "playsound==1.2.2"])
+    else:
+        print("Звука не будет.")
+        play_sound = False
+
+class Sound:
+
+    BUTTON_PRESS = 1
+    START_GAME = 2
+
+    def button_press(self):
+        playsound("sound/button_press.mp3")
+    def start_game(self):
+        playsound("sound/start_game.mp3")
+
+    def play(self, type_sound):
+        if type_sound == Sound.BUTTON_PRESS:
+            Thread(target=self.button_press, daemon=True).start()
+        elif type_sound == Sound.START_GAME:
+            Thread(target=self.start_game, daemon=True).start()
